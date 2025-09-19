727e549ac0851ce4a258ebcd2ab94af8e97d6c08
Romain <r_baruchello@hotmail.com>
2025-09-19 11:49:00 +0200

    ajout des dossiers metier ihm

---
+++ metier/Commande.java
@@ -0,0 +1,13 @@
+package metier;
+
+public class Commande {
+    private String texte;
+
+    public Commande(String texte) {
+        this.texte = texte;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+}

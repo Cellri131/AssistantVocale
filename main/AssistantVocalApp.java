ea0257860e41000ae8bb8ab384c9c3f25e56dd8a
Romain <r_baruchello@hotmail.com>
2025-09-19 12:03:06 +0200

    3 commande simple possible, à revoir

--- main/AssistantVocalApp.java
+++ main/AssistantVocalApp.java
@@ -8,7 +8,7 @@ import metier.TraitementCommande;
 public class AssistantVocalApp {
     public static void main(String[] args) {
         ConsoleIHM ihm = new ConsoleIHM();
-        Voix voix = new Voix();
+        //Voix voix = new Voix();
         TraitementCommande traitement = new TraitementCommande();
 
         System.out.println("=== Assistant Vocal Java ===");
@@ -21,7 +21,7 @@ public class AssistantVocalApp {
             String reponse = traitement.traiter(commande);
 
             ihm.afficher(reponse);
-            voix.parler(reponse);
+            //voix.parler(reponse);
         }
     }
 }
--- metier/TraitementCommande.java
+++ metier/TraitementCommande.java
@@ -4,15 +4,20 @@ public class TraitementCommande {
 
     public String traiter(Commande commande) {
         String texte = commande.getTexte().toLowerCase();
+        String sRet = "";
+        int cpt = 0;
 
         if (texte.contains("bonjour")) {
-            return "Bonjour Romain, comment puis-je vous aider ?";
-        } else if (texte.contains("heure")) {
-            return "Il est " + java.time.LocalTime.now().withNano(0);
-        } else if (texte.contains("date")) {
-            return "Nous sommes le " + java.time.LocalDate.now();
-        } else {
-            return "Désolé, je n'ai pas compris la commande.";
+            sRet += "Bonjour Romain, comment puis-je vous aider ? \n";
         }
+        if (texte.contains("heure")) {
+            sRet += "Il est " + java.time.LocalTime.now().withNano(0) + "\n";
+        } 
+        if (texte.contains("date")) {
+            sRet += "Nous sommes le " + java.time.LocalDate.now()  + "\n";
+        } 
+        //    return "Désolé, je n'ai pas compris la commande. \n";
+        
+        return sRet;
     }
 }
